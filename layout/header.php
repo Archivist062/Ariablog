b95208815170719206fedceef76f12b555253a6a
Zenohate <zenohate@gmail.com>
2017-09-26 13:01:26 +0200

    More design changes, logo change

--- layout/header.php
+++ layout/header.php
@@ -1,9 +1,9 @@
 
-<div class="row" style="background-color:#050505;">
+<div class="row" style="background-color:#22222b;">
 	<div class="col-lg-8 col-lg-offset-2 col-md-12 col-sm-12 ">
 		<div class="row">
-			<div style="text-align:center;color:#D4D4D9;" class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-12">
-				<img src="img/logo.png" alt=""/>
+			<div style="text-align:center;color:#D4D4D9" class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-12">
+				<img style="max-height:40%;" src="img/Logo.png" alt=""/>
 			</div>
 			<div style="text-align:center;color:#D4D4D9;" class="col-lg-6 col-lg-offset-3 sitename col-md-8 col-md-offset-2 col-sm-12">
 				<h1>AriaBlog v1.1</h1>
